08a0525a69b6a7f0dce45ebabb32de45e5dbf4e2
wunexx <sanichgang16@gmail.com>
2026-01-11 21:51:48 +0100

    v0.041
    -added emojis to buttons
    -fixed "You have been invited by someone" text in start

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
-
 const { Telegraf, Markup } = require("telegraf");
-
 const { Pool } = require("pg");
 
 const pool = new Pool({connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false }});
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start(async (ctx) => {
@@ -42,7 +39,7 @@ bot.action("CREATE_REF", async (ctx) => {
 
     const encodedId = Encode(ctx.from.id);
 
-    await ctx.editMessageText(`Succesfully created a referral link! Link: https://t.me/tqa_coin_bot?start=${encodedId}`, BackToMenuKeyboard());
+    await ctx.editMessageText(`Succesfully created a referral link! ⭐\nLink: https://t.me/tqa_coin_bot?start=${encodedId}`, BackToMenuKeyboard());
 });
 
 bot.action("SHOW_REF", async (ctx) => {
@@ -71,8 +68,6 @@ bot.action("BACK_TO_MENU", async (ctx) =>{
 
     await ctx.editMessageText("Welcome to the official TQA meme coin bot! 🚀\nChoose action:", MainMenuKeyboard());
 });
-
-bot.launch().then(() => console.log("Bot is running!")).catch((err) => console.error("Bot launch failed:", err));
 
 async function TryGetRefCount(telegram_id){
     const client = await pool.connect();
@@ -151,10 +146,10 @@ function Decode(str){
 
 function MainMenuKeyboard(){
     return Markup.inlineKeyboard([
-        [Markup.button.callback("Create Referral Link", "CREATE_REF"), Markup.button.callback("My Referral Stats", "SHOW_REF")],
-        [Markup.button.callback("Enter Mini App", "ENTER_APP")],
-        [Markup.button.callback("Referral Leaderboard", "SHOW_LEADERBOARD")],
-        [Markup.button.url("Join News Channel", "https://t.me/+-vL_K7ydtfQ5NWE6"), Markup.button.url("Join Meme Channel", "https://t.me/+R76a4MOb-EQyYjky")]
+        [Markup.button.callback("Create Referral Link✨", "CREATE_REF"), Markup.button.callback("My Referral Stats🪢", "SHOW_REF")],
+        [Markup.button.callback("Enter Mini App🚀", "ENTER_APP")],
+        [Markup.button.callback("Referral Leaderboard🏆", "SHOW_LEADERBOARD")],
+        [Markup.button.url("Join News Channel📃", "https://t.me/+-vL_K7ydtfQ5NWE6"), Markup.button.url("Join Meme Channel🗿", "https://t.me/+R76a4MOb-EQyYjky")]
     ]);
 }
 
