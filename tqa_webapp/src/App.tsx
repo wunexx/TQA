6281216fd6e409918fffe769742f59d8128940a2
wunexx <sanichgang16@gmail.com>
2026-01-12 18:23:04 +0100

    v0.05
    -changed whole project logic. Now there  are 2 folder: 1 for bot and 1 for web app that is made using react.

---
+++ tqa_webapp/src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>About us</li>
+          <li>Our plans</li>
+        </ul>
+      </nav>
+
+      <div className="about-us">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+
+      <div className="our-plans">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      
+      <footer>
+        <p>with love from tqa❤️</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
