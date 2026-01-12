7021f6eb122af4db1f548168e8d299376eaa6f9c
wunexx <sanichgang16@gmail.com>
2026-01-12 18:57:19 +0100

    v0.052
    -added mini app button

--- tqa_bot/index.js
+++ tqa_bot/index.js
@@ -149,7 +149,7 @@ function Decode(str){
 function MainMenuKeyboard(){
     return Markup.inlineKeyboard([
         [Markup.button.callback("✨ Create Referral Link", "CREATE_REF"), Markup.button.callback("🪢 My Referral Stats", "SHOW_REF")],
-        [Markup.button.callback("🚀 Enter Mini App", "ENTER_APP")],
+        [Markup.button.webApp("🚀 Enter Mini App", "https://wunexx.github.io/TQA/")],
         [Markup.button.callback("🏆 Referral Leaderboard", "SHOW_LEADERBOARD")],
         [Markup.button.url("📃 Join News Channel", "https://t.me/+-vL_K7ydtfQ5NWE6"), Markup.button.url("🗿 Join Meme Channel", "https://t.me/+R76a4MOb-EQyYjky")]
     ]);
